3d05b5b3775c3d40a5e97e6a10bae02124fb386c
Dewey:Du <fordewey@gmail.com>
2023-03-05 14:53:44 +0800

    updaate

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -1,13 +1,9 @@
 import { Game } from "./Game";
 import { Tools } from "../system/Tools";
 
-const sourcePreload = {
-    // default: [{ name: "background", url: prefix + "sample.png" }],
-};
-
 export const Config = {
     sourcePreload: Tools.massiveRequire(
-        require["context"]("./../../assets/", true, /\.(mp3|png|jpe?g)$/)
+        require["context"]("./../../assets/", true, /\.(ogg|mp3|png|jpe?g)$/)
     ),
 
     startScene: Game,
--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,5 +1,4 @@
 import * as PIXI from "pixi.js";
-import { App } from "../system/App";
 
 export class Game {
     constructor(texture) {
